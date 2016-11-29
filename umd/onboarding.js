15c46abaab14aabffcdcf20a62e55e1b2c106275
Deepak Grover <deepakgrover333@gmail.com>
2016-11-29 11:28:22 +0530

    minor fixes and build

--- umd/onboarding.js
+++ umd/onboarding.js
@@ -55,7 +55,7 @@ return /******/ (function(modules) { // webpackBootstrap
 /***/ function(module, exports, __webpack_require__) {
 
 	__webpack_require__(1);
-	(function webpackMissingModule() { throw new Error("Cannot find module \"app/app.js\""); }());
+	module.exports = __webpack_require__(1);
 
 
 /***/ },
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,51 @@
-var path = require('path');
-var webpack = require('webpack');
+// var path = require('path');
+// var webpack = require('webpack');
 
-module.exports = {
-	entry: './src/index.js',
+// module.exports = {
+// 	entry: './src/index.js',
+// 	output: {
+// 		path: path.join(__dirname, 'dist/js'),
+// 		publicPath: '/dist/js',
+// 		filename: 'index.js'
+// 	},
+// 	inline: true,
+// 	module: {
+// 		preLoaders: [
+// 				{ test: /\.json$/, exclude: /node_modules/, loader: 'json'},
+// 		],
+// 		loaders: [
+// 			{
+// 				test: /.jsx?$/,
+// 				loader: 'babel-loader',
+// 				exclude: /node_modules/,
+// 				query: {
+// 					presets: ['es2015','stage-0', 'react']
+// 				}
+// 			},
+// 			{
+// 				test: /node_modules\/JSONStream\/index\.js$/,
+// 				loaders: ['shebang', 'babel']
+// 			}
+// 		]
+// 	},
+// };
+
+var path = require('path');
+var LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
+var webpack = require('webpack');
+var env = process.env.NODE_ENV;
+var CHOOSE_CONFIG = process.env.CHOOSE_CONFIG;
+
+// for lib build
+var lib_config = {
+	entry: {
+		app: './src/index.js'
+	},
 	output: {
-		path: path.join(__dirname, 'dist/js'),
-		publicPath: '/dist/js',
+		path: path.join(__dirname, "dist"),
+		publicPath: "/dist/js",
 		filename: 'index.js'
 	},
-	inline: true,
 	module: {
 		preLoaders: [
 				{ test: /\.json$/, exclude: /node_modules/, loader: 'json'},
@@ -29,3 +66,49 @@ module.exports = {
 		]
 	},
 };
+
+// for examples build
+var examples_config = {
+	entry: {
+		main: './src/index.js'
+	},
+	output: {
+		path: path.join(__dirname, "dist"),
+		publicPath: "/dist/js/",
+		filename: 'index.js'
+	},
+	module: {
+		preLoaders: [
+				{ test: /\.json$/, exclude: /node_modules/, loader: 'json'},
+		],
+		loaders: [
+			{
+				test: /.jsx?$/,
+				loader: 'babel-loader',
+				exclude: /node_modules/,
+				query: {
+					presets: ['es2015','stage-0', 'react']
+				}
+			},
+			{
+				test: /node_modules\/JSONStream\/index\.js$/,
+				loaders: ['shebang', 'babel']
+			}
+		]
+	},
+};
+
+var final_config;
+switch(CHOOSE_CONFIG) {
+	case 'UMD':
+		final_config = umd_config;
+	break;
+	case 'LIB':
+		final_config = lib_config;
+	break;
+	case 'EXAMPLES':
+	default:
+		final_config = examples_config;
+	break;
+}
+module.exports = final_config;
