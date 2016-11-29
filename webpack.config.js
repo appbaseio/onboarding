446603c8cf00d919290e3403b0047ca5cb3581e6
Deepak Grover <deepakgrover333@gmail.com>
2016-11-29 11:29:07 +0530

    minor fixes and build

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,3 @@
-// var path = require('path');
-// var webpack = require('webpack');
-
-// module.exports = {
-// 	entry: './src/index.js',
-// 	output: {
-// 		path: path.join(__dirname, 'dist/js'),
-// 		publicPath: '/dist/js',
-// 		filename: 'index.js'
-// 	},
-// 	inline: true,
-// 	module: {
-// 		preLoaders: [
-// 				{ test: /\.json$/, exclude: /node_modules/, loader: 'json'},
-// 		],
-// 		loaders: [
-// 			{
-// 				test: /.jsx?$/,
-// 				loader: 'babel-loader',
-// 				exclude: /node_modules/,
-// 				query: {
-// 					presets: ['es2015','stage-0', 'react']
-// 				}
-// 			},
-// 			{
-// 				test: /node_modules\/JSONStream\/index\.js$/,
-// 				loaders: ['shebang', 'babel']
-// 			}
-// 		]
-// 	},
-// };
-
 var path = require('path');
 var LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
 var webpack = require('webpack');
